f2a80c4fabd1812310f7fd5c0970b9db7da576d3
Sofie <sohe0002@stud.kea.dk>
2024-05-20 20:45:19 +0200

    kurv komponent

--- javascript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenu = document.querySelector(".burger-menu");
-  const nav = document.querySelector("nav");
-
-  burgerMenu.addEventListener("click", function () {
-    nav.classList.toggle("active");
-  });
-});
